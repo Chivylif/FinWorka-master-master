38f035a605e1dc4f8ddabb116dd26b289ff07b23
chivylif <chivy4gud@gmail.com>
2022-08-28 17:28:34 +0100

    first commit

---
+++ src/components/Characters.js
@@ -0,0 +1,4 @@
+import React from "react";
+export default function Character() {
+  return <h1>Characters</h1>;
+}
---
+++ src/components/Comments.js
@@ -0,0 +1,80 @@
+import Form from "react-bootstrap/Form";
+import React from "react";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import "../App.css"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import book from "../book.avif";
+
+
+export default function Comments() {
+    const [comments, setComments] = useState([]);
+    const {id, name} = useParams();
+    const [comm, setComm] = useState({});
+    let current;
+
+    useEffect(() => {
+        getComments();
+    }, [])
+
+    const getComments = () => {
+         axios
+           .get(`http://findworker-app.herokuapp.com/api/comment/get_comments.php?id=${id}`
+           )
+           .then(function (response) {
+             console.log(response.data);
+             let reversed = [];
+             if (Object.keys(response.data).length > 1) {
+                reversed = [...response.data].reverse();
+                setComments(reversed);
+             }
+             setComments(response.data);
+           });
+    }
+
+    const postComment = (e) => {
+        e.preventDefault();
+        console.log("Hello");
+        axios.post(`http://findworker-app.herokuapp.com/api/comment/create.php?id=${id}`, comm);
+        //   .then(function (response) {});
+        setTimeout(() => window.location.reload(), 3000);
+    }
+
+    const setCom = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setComm(prev => ({...prev, [name]: value}));
+        current = value;
+    }
+
+    return (
+        <div className="con">
+            <img src={book} alt="Random book"/>
+            <h4 className="mt-2">Book Title: {name}</h4>
+            <hr className="line" />
+            {comments.length > 0 ? 
+                comments.map((comment, key) => (
+                <div>
+                    <p className="wrap">{comment.comment}</p>
+                    <p>Commenter: Dozie</p>
+                    <hr />
+                </div>
+                )) : <p>No comments yet</p>
+            }
+            <Form onSubmit={postComment}>
+                <FloatingLabel controlId="floatingTextarea2" label="Comment">
+                    <Form.Control
+                        as="textarea"
+                        placeholder="Leave a comment here"
+                        style={{ height: '100px' }}
+                        name="comment"
+                        onChange={setCom}
+                        value={current}
+                    />
+                </FloatingLabel>
+                <button className="btn btn-primary btn1">Post</button>
+            </Form>
+        </div>
+    )
+}
